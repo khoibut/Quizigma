bccad9c28016c02b7354398f61d2d3b76e233adb
Shovul <140464903+Shovul@users.noreply.github.com>
2024-12-18 13:59:08 +0700

    Navigating to sign in and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/images/react.svg'
 import viteLogo from '/vite.svg'
 import './assets/styles/App.css'
-import AccountForm from '../AccountSystem/SignIn.jsx'
+import SignIn from './components/AccountSystem/SignIn.jsx'
+import SignUp from './components/AccountSystem/SignUp.jsx'
+import LandingPage from './components/LandingPage/LandingPage.jsx';
+
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AccountForm value={"SIGN IN"} inputs={['email', 'password']} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path='signin' element={<SignIn />} />
+          <Route path='signup' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,3 +1,25 @@
+// import OnlineIcon from '../../assets/images/OnlineIcon.png'
+// import PlayIcon from '../../assets/images/PlayIcon.png'
+import landingPageBg from '../../assets/images/landingPageBg.png'
+import Title from './Title.jsx'
+import MainSidebar from './MainSidebar.jsx'
+import '../../assets/styles/App.css'
+// import AccountForm from '../AccountSystem/AccountForm.jsx'
+
+
 function LandingPage() {
-    
+    return (
+        <>
+            <Title />
+            <div class="flex w-full h-auto">
+                <div class="flex grow justify-end h-auto">
+                    <MainSidebar />
+                    <div class="h-auto w-full">
+                        <img src={landingPageBg} alt="Welcome to Quizigma" class="w-full object-fit" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
+export default LandingPage
