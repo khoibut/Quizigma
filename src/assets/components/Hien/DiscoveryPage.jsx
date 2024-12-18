8f8a26d61c128750e8b94b81b3b0ae4c203d8bd2
Vantom0v0 <vantom572005@gmail.com>
2024-12-18 23:57:22 +0700

    Discovery Page: add the "Add button", add some animations while hovering the modes

--- src/assets/components/Hien/DiscoveryPage.jsx
+++ src/assets/components/Hien/DiscoveryPage.jsx
@@ -3,6 +3,7 @@ import PlayIcon from "./Play icon.png"
 import DiscoveryIcon from "./DiscoveryIcon.png"
 import FolderIcon from "./FolderIcon.png"
 import LibraryIcon from "./LibraryIcon.png"
+import AddIcon from "./AddIcon.png"
 
 function Discovery(){
     
@@ -29,9 +30,12 @@ function Discovery(){
                 <div class="bg-white max-w-[420px] grow shrink flex-2">
                     {/* User info */}
                     <div class="max-w-[420px] max-h-[300px] grow  min-w-[100px] min-h-[150px] shrink flex items-center justify-center gap-5 flex-wrap">
-                        <div class="rounded-full bg-white border-[5px] border-teal-500 w-[100px] h-[100px] mt-5"></div>
-                        <div class="rounded-r-xl bg-teal-500 max-w-[250px] max-h-[200px] grow mb-5 flex justify-center items-center font-bold flex-wrap">
-                            *Example name Example name
+                        <div class="rounded-full bg-white border-[5px] border-teal-500
+                         w-[100px] h-[100px] mt-5"></div>
+                        <div class="rounded-r-xl bg-teal-500 max-w-[250px]
+                         max-h-[200px] grow mb-6 flex justify-center items-center 
+                         font-bold flex-wrap">
+                            Hubert Blaine Wolfe­schlegel­stein­hausen­berger­dorff Sr
                         </div>
                     </div>
 
@@ -48,7 +52,7 @@ function Discovery(){
 
                             {/* Library button */}
                             <button class="relative rounded-full shadow-xl bg-[#487FA7] font-bold text-white hover:bg-sky-500 hover:shadow-none ease-in 
-                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center
+                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center hover:animate-bounce
                             max-w-[140px] max-h-[75px] grow m-[1px]">
                                 <img src={LibraryIcon} alt="Online Icon" class="w-10 h-10 mb-1"/>
                                 <span class="text-sm">Library</span>
@@ -58,7 +62,7 @@ function Discovery(){
                         <div class="flex flex-wrap justify-evenly grow">
                             {/* <!-- Online Button --> */}
                             <button class="relative rounded-full shadow-xl bg-[#487FA7] font-bold text-white hover:bg-sky-500 hover:shadow-none ease-in 
-                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center justify-center 
+                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center justify-center hover:animate-bounce
                             max-w-[140px] max-h-[75px] grow m-[1px]">
                                 <img src={OnlineIcon} alt="Online Icon" class="w-10 h-10 mb-1"/>
                                 <span class="text-sm">Online</span>
@@ -66,7 +70,7 @@ function Discovery(){
 
                             {/* <!-- Play Button --> */}
                             <button class="relative rounded-full shadow-xl bg-[#487FA7] font-bold text-white hover:bg-sky-500 hover:shadow-none ease-in 
-                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center justify-center 
+                            duration-200 text-base min-w-[100px] min-h-[60px] shrink flex flex-col items-center justify-center hover:animate-bounce
                             max-w-[140px] max-h-[75px] grow m-[1px]">
                                 <img src={PlayIcon} alt="Play Icon" class="w-10 h-10 mb-1"/>
                                 <span class="text-sm">Play</span>
@@ -74,8 +78,21 @@ function Discovery(){
                         </div>
                     </div>
 
+                    {/* Add button */}
+                    <div class="flex flex-wrap mt-[100px]">
+                        <div class="bg-[#006B2D] w-[80px] h-[80px] flex justify-center items-center">
+                            <img src={AddIcon} alt="Add Icon" class="w-[50px] h-[50px]" />
+                        </div>
+                        <button class="bg-[#21DC6F] h-[80px] max-w-[270px] grow min-w-[90px] 
+                        shrink rounded-r-full flex justify-center items-center 
+                        hover:border-green-300 hover:border-[10px] text-xl hover:text-3xl">
+                            <div class="font-bold text-white">Add</div>
+                        </button>
+            
+                    </div>
+
                     {/* Setting */}
-                    <div class="flex justify-center items-center text-3xl mt-[387px]">
+                    <div class="flex justify-center items-center text-3xl mt-[157px]">
                         <button id="settings" class="bg-white hover:bg-gray-600 hover:text-white ease-in duration-100 rounded-full
                         min-w-[100px] min-h-[60px] shrink max-w-[200px] max-h-[75px] grow font-bold m-[1px]">
                             Settings
