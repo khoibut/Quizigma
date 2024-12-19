c353fa2013245db5bb3fe3ecd85065c933b32576
Shovul <140464903+Shovul@users.noreply.github.com>
2024-12-19 11:38:41 +0700

    QuizDisplay and quiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import './assets/styles/App.css'
 import SignIn from './components/AccountSystem/SignIn.jsx'
 import SignUp from './components/AccountSystem/SignUp.jsx'
 import LandingPage from './components/LandingPage/LandingPage.jsx';
+import AddQuestion from './components/QuestionSystem/AddQuiz.jsx'
+import QuestionDisplay from './components/QuestionSystem/QuestionDisplay.jsx'
+
 
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<LandingPage />} />
+          <Route path='questionlist' element={
+        <QuestionDisplay />} />
           <Route path='signin' element={<SignIn />} />
           <Route path='signup' element={<SignUp />} />
         </Routes>
