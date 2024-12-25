31445f36897376a6aff8ed8c5c72b704c91ec99d
Shovul <140464903+Shovul@users.noreply.github.com>
2024-12-25 23:00:49 +0700

    Add image (no crop feature)

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,11 @@ import QuestionDisplay from './components/QuestionSystem/QuestionDisplay.jsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Discovery from './assets/components/Hien/DiscoveryPage.jsx'
 import Test from './assets/components/Hien/Test.jsx'
-
 import "./index.css"
+import HostGame from './components/Game/HostGame.jsx'
+import AddImage from './components/PopUp/AddImage.jsx'
+import AddQuiz from './components/QuestionSystem/AddQuiz.jsx'
+
 function App() {
 
   return (
@@ -19,14 +22,15 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<LandingPage />} />
-          <Route path='questionlist' element={
-        <QuestionDisplay />} />
+          <Route path='questionlist' element={<QuestionDisplay />} />
+          <Route path='host' element={<HostGame />} />
           <Route path='signin' element={<SignIn />} />
           <Route path='signup' element={<SignUp />} />
+          <Route path='addquiz' element={<AddQuiz />} />
         </Routes>
       </BrowserRouter>
 
-      <Discovery />
+      {/* <Discovery /> */}
       {/* <LandingPage /> */}
       {/* <Test /> */}
       {/* <TemplateCard /> */}
--- src/components/AccountSystem/SignUp.jsx
+++ src/components/AccountSystem/SignUp.jsx
@@ -2,19 +2,105 @@ import Title from "../LandingPage/Title"
 import { NavLink } from "react-router"
 import '../../assets/styles/App.css'
 import { useState } from "react"
+import { useRef } from "react"
 import axios from "axios"
+import Modal from "react-modal"
 
 
 function SignUp() {
     // const axios = require('axios').default
-    const [user, setUser] = useState({})
+    // const [user, setUser] = useState({})
+    let usernameInput = useRef()
+    let emailInput = useRef()
+    let passwordInput = useRef()
+    let repasswordInput = useRef()
+    let usernameError = useRef()
+    let emailError = useRef()
+    let passwordError = useRef()
+    let repasswordError = useRef()
+    let lengthError = useRef()
+    let numberError = useRef()
+    let lowerError = useRef()
+    let upperError = useRef()
+    let specialError = useRef()
     
     function addAccount() {
+        let flag = true
+        let passwordFlag = true
+        const user = {
+            username: usernameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value
+        }
+
+        // password check
+        if(user.password.length < 8 || user.password === '') {
+            lengthError.style.display = 'block'
+            passwordFlag = false
+        }
+        else {
+            lengthError.style.display = 'none'
+        }
+
+        if(user.password == user.password.toLowerCase() || user.password === '') {
+            upperError.style.display = 'block'
+            passwordFlag = false
+        }
+        else {
+            upperError.style.display = 'none'
+        }
         
-        axios.post('https://1e146b0b-03dd-4484-8c90-6ed84cfa86df.mock.pstmn.io/api/acc', {user})
-        .then(function (token) {
-            console.log(token); 
-        })
+        if(user.password == user.password.toUpperCase() || user.password === '') {
+            lowerError.style.display = 'block'
+            passwordFlag = false
+        }
+        else {
+            lowerError.style.display = 'none'
+        }
+
+        if(user.password.split('').every(char => {return (char >= '0' && char <= '9') ? false : true}) || user.password === '') {
+            numberError.style.display = 'block'
+            passwordFlag = false
+        }
+        else {
+            numberError.style.display = 'none'
+        }
+
+        if(!(user.password.includes('$') || user.password.includes('&') || user.password.includes('%') || user.password.includes('!') || user.password.includes('@') || user.password.includes('?')) || user.password === '') {
+            specialError.style.display = 'block'
+            passwordFlag = false
+        }
+        else {
+            specialError.style.display = 'none'
+        }
+        //if theres wrong display
+        if(passwordFlag) {
+            passwordError.style.display = 'none'
+        }
+        else {
+            passwordError.style.display = 'block'
+            flag = false
+        }
+
+        // check confirm password
+        if(repasswordInput.value !== user.password) {
+            repasswordError.style.display = "flex"
+            flag = false
+        }
+        else {
+            repasswordError.style.display = "none"
+        }
+
+        if(flag) {
+            axios.post('https://quizigmaapi.onrender.com/api/v1/acc', user)
+            .then(function (response) {
+                console.log(response);
+                alert(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
     }
 
     return (
@@ -24,16 +110,39 @@ function SignUp() {
                 <div class="self-start text-2xl font-extrabold m-10 mb-2     text-white">SIGN UP</div>
                 <div class="flex flex-col w-3/5 lg:w-1/2 m-2">
                     <label class="mt-2 text-xl" for="username">Username</label>
-                    <input onChange={(current) => {setUser({username: current.target.value})}} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="username" name="username" placeholder="Enter your email" />
+                    <input ref={(current) => (usernameInput = current)} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="username" name="username" placeholder="Enter your username" />
+                    <div ref={(current) => (usernameError = current)} class="text-rose-500 font-bold hidden mt-1 gap-2 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(244, 63, 94)"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                        <span>Username had already been taken</span>
+                    </div>
         
                     <label class="mt-2 text-xl" for="email">Email</label>
-                    <input onChange={(current) => {setUser({email: current.target.value})}} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="email" name="email" placeholder="Enter your email" />
+                    <input ref={(current) => (emailInput = current)} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="email" name="email" placeholder="Enter your email" />
+                    <div ref={(current) => (emailError = current)} class="text-rose-500 font-bold hidden mt-1 gap-2 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(244, 63, 94)"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                        <span>Email has already been taken</span>
+                    </div>
                     
                     <label class="mt-2 text-xl" for="password">Password</label>
-                    <input onChange={(current) => {setUser({password: current.target.value})}} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="password" name="password" placeholder="Enter your password" />
+                    <input ref={(current) => (passwordInput = current)} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="password" name="password" placeholder="Enter your password" />
+                    <div ref={(current) => (passwordError = current)} class="text-rose-500 font-bold mt-1 hidden">
+                        <div class="flex gap-2 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(244, 63, 94)"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                            <span>Warning: Password must contain</span>
+                        </div>
+                        <div ref={(current) => (lengthError = current)} class="hidden">• At least 8 characters long</div>
+                        <div ref={(current) => (numberError = current)} class="hidden">• A number</div>
+                        <div ref={(current) => (lowerError = current)} class="hidden">• A lowecase letter</div>
+                        <div ref={(current) => (upperError = current)} class="hidden">• A uppercase letter</div>
+                        <div ref={(current) => (specialError = current)} class="hidden">• A specialize letter [$&%!@?]</div>
+                    </div>
                     
                     <label class="mt-2 text-xl" for="repassword">Re-enter password</label>
-                    <input class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="repassword" name="repassword" placeholder="Enter your password" />
+                    <input ref={(current) => (repasswordInput = current)} class="rounded-md p-3 outline-none hover:outline-blue-950 hover:outline-offset-2" type="text" id="repassword" name="repassword" placeholder="Confirm your password" />
+                    <div ref={(current) => (repasswordError = current)} class="text-rose-500 font-bold hidden mt-1 gap-2 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(244, 63, 94)"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                        <span>Password must match</span>
+                    </div>
         
                     <button class="mt-5 self-end px-10 py-2 rounded-full bg-red-400 text-white hover:bg-rose-500 hover:scale-105 transition-all" onClick={addAccount}>Continue</button>
                 </div>
